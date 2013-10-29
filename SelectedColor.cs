f580ed2056b670675ae41e4719e80ffbd9e7ccb3
deqnchugunov <deqnchugunov@gmail.com>
2013-10-29 20:11:10 +0200

    Create SelectedColor.cs

---
+++ SelectedColor.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPaint
+{
+    public enum SelectedColor
+    {
+        Black,
+        Red,
+        Yellow,
+        Blue,
+        White,
+        Green,
+        Orange,
+        Purple
+    }
+}
